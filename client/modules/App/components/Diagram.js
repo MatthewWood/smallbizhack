306b7e875be96413375a5e2e88c707d3e5f10d68
mdave16 <mdave3927@gmail.com>
2019-02-24 04:09:48 +0000

    Implement CSS without CSS

--- client/modules/App/components/Diagram.js
+++ client/modules/App/components/Diagram.js
@@ -4,15 +4,47 @@ import bg from '../header-bk.png';
 import PropTypes from 'prop-types';
 import Tree from 'react-tree-graph';
 
-// import styles from"react-tree-graph/dist/style.css";
-import "./Diagram.css";
-
+// import "react-tree-graph/dist/style.css";
+import styles from "./Diagram.css";
+const stylesIn = (_) => ({
+		nodeCircle: {
+		 fill :'white',
+		 stroke:'black',
+	 	},
+		pathLink: {
+			fill:'none',
+			stroke:'black',
+		},
+		blocked: {
+			fill: 'red',
+			stroke: 'red',
+		},
+		ready: {
+			fill: 'blue',
+			stroke: 'blue',
+		},
+		inprogress: {
+			fill: 'orange',
+			stroke: 'orange',
+		},
+		complete: {
+			fill: 'green',
+			stroke: 'green',
+		}
+});
 const status = (status) => {
 
  return	{
-	className: 'node ' +  status
+	className: 'node ' +  status,
+	style: style(status)
 }}
 
+const style = (status) => {
+	return status === "complete" ? {fill: 'green', stroke: 'green'} :
+	status === "in progres" ? {fill: 'orange', stroke: 'orange'} :
+	status === "ready" ? {fill: 'blue', stroke: 'blue'} :
+	status === "blocked" ?  {fill: 'red', stroke: 'red'} : {}
+}
 const parseMaterial = (material) => ({
 	_id: material._id + Math.random().toString(),
 	name: material.name,
