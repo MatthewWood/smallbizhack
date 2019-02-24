34376e4bee017240fbee82bb305929e18aaf749c
mdave16 <mdave3927@gmail.com>
2019-02-24 05:57:30 +0000

    Better CSS for diagram

--- client/modules/App/components/Diagram.js
+++ client/modules/App/components/Diagram.js
@@ -4,7 +4,7 @@ import bg from '../header-bk.png';
 import PropTypes from 'prop-types';
 import Tree from 'react-tree-graph';
 
-import "react-tree-graph/dist/style.css";
+// import "react-tree-graph/dist/style.css";
 // import styles from "./Diagram.css";
 
 const status = (status) => {
@@ -13,18 +13,31 @@ const status = (status) => {
 	className: status,
 	style: style(status),
 }}
-
+const statusColor = (status) => {
+	return status === "complete" ? 'green':
+	status === "in progres" ? 'orange' :
+	status === "ready" ? 'blue' :
+	status === "blocked" ?  'red' : 'black';
+}
 const style = (status) => {
-	return status === "complete" ? {fill: 'green', stroke: 'green', r: '25'} :
-	status === "in progres" ? {fill: 'orange', stroke: 'orange', r: '25'} :
-	status === "ready" ? {fill: 'blue', stroke: 'blue', r: '25'} :
-	status === "blocked" ?  {fill: 'red', stroke: 'red', r: '25'} : {}
+	return status === "complete" ? {fill: 'green', stroke: 'green'} :
+	status === "in progres" ? {fill: 'orange', stroke: 'orange'} :
+	status === "ready" ? {fill: 'blue', stroke: 'blue'} :
+	status === "blocked" ?  {fill: 'red', stroke: 'red'} : {}
 }
 const parseMaterial = (material) => ({
 	_id: material._id + Math.random().toString(),
 	name: material.name,
 	circleProps: status('complete'),
-	textProps: {style: {dx: -80, dy: 50, fontSize: 'x-large', fontWeight: '900'}}
+	textProps: {style: {fontSize: 'x-large', fontWeight: '900', textTransform: 'capitalize'
+	}},
+	pathProps: {
+		style: {
+				fill:'none',
+				stroke: statusColor('complete'),
+				strokeWidth: '15px'
+		}
+	}
 })
 
 const name = (id) => {
@@ -41,8 +54,17 @@ const data = (treeData) => {
 		_id: treeData.myId,
     name: name(treeData.myId),
 		// gProps: status(treeData.status),
-		textProps: {dx: -80, dy: 50, style: {dX: -80, dY: 50, fontSize: 'x-large', fontWeight: '900'}},
+		textProps: {dx: -80, dy: 50, style: {textTransform: 'capitalize',
+		fontSize: 'x-large',
+		fontWeight: '900' }},
 		circleProps: status(treeData.status),
+		pathProps: {
+			style: {
+					fill:'none',
+					stroke: statusColor(treeData.status),
+					strokeWidth: '15px'
+			}
+		},
 		children: treeData.components.map(data).concat( treeData.materials.map(parseMaterial))
 	}
 };
@@ -61,16 +83,19 @@ export function Diagram(props) {
 			pathProps={{
 				className: 'link',
 				style: {
-					pathLink: {
 						fill:'none',
 						stroke:'black',
-					}
 				}
 			}}
+			margins={{ bottom : 10, left : 20, right : 180, top : 10}}
 			textProps={{
-				dx: -80,
-				dy: 50
-			}}/>
+				textTransform: 'capitalize',
+				fontSize: 'x-large',
+				fontWeight: '900'
+			}}
+			nodeOffset={10}
+			nodeRadius={20}
+			/>
 			// </div>
   );
 }
