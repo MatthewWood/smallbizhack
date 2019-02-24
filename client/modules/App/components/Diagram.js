772aa07f0c4ba4b52f43a9e9b0c62c8c83be0586
mdave16 <mdave3927@gmail.com>
2019-02-24 00:39:03 +0000

    Implement the rest of the diagram without CSS still

--- client/modules/App/components/Diagram.js
+++ client/modules/App/components/Diagram.js
@@ -31,49 +31,42 @@ const styles = () => ({
 	}
 
 });
-// .red-node {
-// 	fill: red;
-// 	stroke: red;
-// }
-//
-// let data = {
-//     name: 'Parent',
-//     children: [{
-//         name: 'Child One'
-//     }, {
-//         name: 'Child Two',
-// 				gProps: {
-// 					className: {styles().}
-// 				}
-//     }]
-// };
+// {"components":{
+// "_id":"5c717963814d3324394b91a5",
+// "status":"blocked",
+// "isProduct":true,
+// "__v":0,
+// "materials":[],
+// "components":[]}}
+let status = (status) => {
+	console.log(status);
 
-let data2 = (classes) => ({
-    name: 'Dog House',
-    children: [{
-        name: 'Roof'
-    }, {
-        name: 'Wall',
-        // gProps: { className: classes.ready }
-    }, {
-        name: 'Wall',
-        // gProps: { className: classes.ready }
-    }, {
-        name: 'Wall',
-        // gProps: { className: classes.ready }
-				children: [{
-					name: 'Wood',
-					gProps: {
-						className: 'ready node'
-					}
-				},{
-					name: 'Wood',
-					gProps: {
-						className: 'blocked node'
-					}
-				}]
-    }]
-});
+ return	{
+	className: 'node ' +  status
+}}
+
+let parseMaterial = (material) => ({
+	name: material.name,
+	gProps: status('complete')
+})
+
+let name = (id) => {
+	return id === "1" ? "Roof" :
+	id === "2" ? "Walls" :
+	id === "3" ? "Door" :
+	id === "4" ? "House Component" :
+	id === "5" ? "Roof" : "Random Piece"
+}
+
+
+let data = (treeData) => {
+	console.dir(treeData);
+	return {
+    name: name(treeData.myId),
+		gProps: status(treeData.status),
+		children: treeData.components.map(data).concat( treeData.materials.map(parseMaterial))
+	}
+};
 //
 // gProps: {
 // 			className: {classes.blocked}
@@ -83,13 +76,114 @@ let data2 = (classes) => ({
 // 	data={data}
 // 	height={200}
 // 	width={400}/>
+let myFakeData = {
+  "components":{
+    "_id":"5c71c414522862eb41807410",
+    "myId":"5",
+    "status":"blocked",
+    "isProduct":true,
+    "__v":0,
+    "materials":[
+      {
+        "_id":"5c71c414522862eb41807407",
+        "name":"wooden floor",
+        "__v":0
+      }
+    ],
+    "components":[
+      {
+        "_id":"5c71c414522862eb4180740f",
+        "myId":"4",
+        "status":"blocked",
+        "isProduct":false,
+        "__v":0,
+        "materials":[
+
+        ],
+        "components":[
+          {
+            "_id":"5c71c414522862eb4180740e",
+            "myId":"3",
+            "status":"ready",
+            "isProduct":false,
+            "__v":0,
+            "materials":[
+              {
+                "_id":"5c71c414522862eb41807409",
+                "name":"wooden front",
+                "__v":0
+              },
+              {
+                "_id":"5c71c414522862eb4180740a",
+                "name":"entry cutout",
+                "__v":0
+              }
+            ],
+            "components":[
+
+            ]
+          },
+          {
+            "_id":"5c71c414522862eb4180740d",
+            "myId":"2",
+            "status":"ready",
+            "isProduct":false,
+            "__v":0,
+            "materials":[
+              {
+                "_id":"5c71c414522862eb41807408",
+                "name":"wooden side",
+                "__v":0
+              },
+              {
+                "_id":"5c71c414522862eb41807408",
+                "name":"wooden side",
+                "__v":0
+              },
+              {
+                "_id":"5c71c414522862eb41807408",
+                "name":"wooden side",
+                "__v":0
+              }
+            ],
+            "components":[
+
+            ]
+          }
+        ]
+      },
+      {
+        "_id":"5c71c414522862eb4180740c",
+        "myId":"1",
+        "status":"ready",
+        "isProduct":false,
+        "__v":0,
+        "materials":[
+          {
+            "_id":"5c71c414522862eb4180740b",
+            "name":"wooden roof",
+            "__v":0
+          },
+          {
+            "_id":"5c71c414522862eb4180740b",
+            "name":"wooden roof",
+            "__v":0
+          }
+        ],
+        "components":[
+
+        ]
+      }
+    ]
+  }
+}
 
 export function Diagram(props) {
   const { classes, treeData } = props;
 
   return (
 		<Tree
-			data={data2(treeData)}
+			data={data(myFakeData.components)}
 			height={500}
 			width={800}
 			animated/>
